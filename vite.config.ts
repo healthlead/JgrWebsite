06f44f2385aa928de20006386a8dfb9d8847e41c
healthlead <contact@healthlead.io>
2025-07-07 20:39:48 -0400

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  // 👇 ADD THIS LINE
+  base: "/JrgWebsite/",
+
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -25,7 +28,8 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    // 👇 CHANGE THIS LINE
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
   },
   server: {
