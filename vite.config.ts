cbf0771f61570fc00947adf9378ceec5d8ae750c
healthlead <contact@healthlead.io>
2025-07-07 20:43:13 -0400

    vite config 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
   // 👇 ADD THIS LINE
-  base: "/JrgWebsite/",
+  base: "/",
 
   plugins: [
     react(),
